55f3021db5c19a995341205386b4d193d8ee674c
James Netherton <jamesnetherton@gmail.com>
2022-02-16 15:05:47 +0000

    WIP

--- test.sh
+++ test.sh
@@ -11,22 +11,28 @@ TEST_FAILURES=()
 RED='\033[0;31m'
 CLEAR='\033[0m'
 
-for MODULE in ${TEST_MODULES//,/ }; do
-./mvnw ${MAVEN_ARGS} clean verify \
-    -Dformatter.skip -Dimpsort.skip \
-    -f "${MODULE}/pom.xml"
+# for MODULE in ${TEST_MODULES//,/ }; do
+# ./mvnw ${MAVEN_ARGS} clean verify \
+#     -Dformatter.skip -Dimpsort.skip \
+#     -f "${MODULE}/pom.xml"
 
 
-if [[ $? -ne 0 ]]; then
-    TEST_FAILURES[${#TEST_FAILURES[@]}]=${MODULE}
-fi
+# if [[ $? -ne 0 ]]; then
+#     TEST_FAILURES[${#TEST_FAILURES[@]}]=${MODULE}
+# fi
 
+# done
+
+# if [[ ${#TEST_FAILURES[@]} -gt 0 ]]; then
+#     echo -e "\n\n${RED}Build errors were encountred in the following projects:${CLEAR}\n\n"
+#     for FAILURE in ${TEST_FAILURES[@]}; do
+#         echo -e "* ${RED}${FAILURE}${CLEAR}"
+#     done
+#     echo -e "\n\n${RED}Check build logs for further information.${CLEAR}"
+# fi
+
+
+
+for i in foo bar cheese; do
+echo -e "$i\n"
 done
-
-if [[ ${#TEST_FAILURES[@]} -gt 0 ]]; then
-    echo -e "\n\n${RED}Build errors were encountred in the following projects:${CLEAR}\n\n"
-    for FAILURE in ${TEST_FAILURES[@]}; do
-        echo -e "* ${RED}${FAILURE}${CLEAR}"
-    done
-    echo -e "\n\n${RED}Check build logs for further information.${CLEAR}"
-fi
